ec75f02beef71d7eb63ad99c7f14e086a7c5661c
tanishqvyas <tanishqvyas069@gmail.com>
2021-10-24 13:11:10 +0530

    audiobook

--- generate_audio.py
+++ generate_audio.py
@@ -18,12 +18,13 @@ def generate_audio(audioId, text_prompt, encoder, synthesizer, vocoder, referenc
     # Fetching the reference audio
     in_fpath = Path(reference_audio_path)
 
-    print("[2] Generating the Audio")
+    print("[2] Generating the Audio Features")
     preprocessed_wav = encoder.preprocess_wav(in_fpath)
     original_wav, sampling_rate = librosa.load(in_fpath)
     preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
     embed = encoder.embed_utterance(preprocessed_wav)
 
+    print("[3] Generating Audio")
     with io.capture_output() as captured:
         specs = synthesizer.synthesize_spectrograms([text_prompt], [embed])
 
@@ -31,7 +32,7 @@ def generate_audio(audioId, text_prompt, encoder, synthesizer, vocoder, referenc
     generated_wav = np.pad(
         generated_wav, (0, synthesizer.sample_rate), mode="constant")
 
-    print("[3] Saving the Audio")
+    print("[4] Saving the Audio")
     # Saving the Generated Audio file
     audio_file_path = os.path.join("static", "audio" + audioId + ".wav")
     sf.write(audio_file_path, generated_wav, synthesizer.sample_rate)
@@ -39,6 +40,42 @@ def generate_audio(audioId, text_prompt, encoder, synthesizer, vocoder, referenc
     return audio_file_path
 
 
-def generate_audiobook_file(audioId, text_prompt, encoder, synthesizer, vocoder, reference_audio_path):
+def generate_audiobook_file(audioId, text_prompt, encoder, synthesizer, vocoder, reference_audio_path, WORD_SKIP_COUNT = 17):
 
-    pass
+
+    print("[1] Fetching Reference Audio")
+    # Fetching the reference audio
+    in_fpath = Path(reference_audio_path)
+
+    print("[2] Generating the Audio Features")
+    preprocessed_wav = encoder.preprocess_wav(in_fpath)
+    original_wav, sampling_rate = librosa.load(in_fpath)
+    preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
+    embed = encoder.embed_utterance(preprocessed_wav)
+
+    print("[3] Generating the Speech")
+    audiobookArray = np.array([])
+    # Splitting the text prompt sentence wise
+    words = text_prompt.split(" ")
+    for wordIdx in range(0, len(words), WORD_SKIP_COUNT):
+
+        sentence = " ".join(words[wordIdx : wordIdx+WORD_SKIP_COUNT])
+        print(f"[Current Sentence] {sentence} \n")
+
+        with io.capture_output() as captured:
+            specs = synthesizer.synthesize_spectrograms([sentence], [embed])
+
+        generated_wav = vocoder.infer_waveform(specs[0])
+        audiobookArray = np.concatenate((audiobookArray, generated_wav), axis=0)
+    
+    # Padding
+    # audiobookArray = np.pad(
+    #     audiobookArray, (0, synthesizer.sample_rate), mode="constant")
+
+    print("\n [5] Saving generated audiobook")
+    # Saving the Generated Audio file
+    audio_file_path = os.path.join("static", "audio" + audioId + ".wav")
+    sf.write(audio_file_path, audiobookArray, synthesizer.sample_rate)
+
+    return audio_file_path
+
